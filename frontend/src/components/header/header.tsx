8ea2f8e19e23def4d4156368f8a6fca85007c415
dLuxKid <adetunjimarvellous09@gmail.com>
2024-04-04 16:53:35 +0100

    implemented shadcn and completed the homepage

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../ui/navigation-menu";
 
 const nav_items = [
   { item: "Home", route: "/" },
@@ -9,25 +18,40 @@ const nav_items = [
 
 export default function Header() {
   return (
-    <nav className="flex items-center justify-center px-[5%] h-24">
+    <NavigationMenu className="flex items-center justify-center px-[5%] h-24">
       <div className="w-full flex items-center justify-between gap-6">
-        <h1 className="text-main-green">Dokita</h1>
-        <ul className="flex items-center justify-center gap-6">
-          {nav_items.map((item, i) => (
-            <Link key={i} href={item.route}>
-              <li className="text-lg md:text-xl font-normal hover:text-main-green text-black">
-                {item.item}
-              </li>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href={"/"}>
+              <h1 className="text-primary">Dokita</h1>
             </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        <NavigationMenuList className="flex items-center justify-center gap-6">
+          {nav_items.map((item, i) => (
+            <NavigationMenuItem key={i}>
+              <Link
+                href={item.route}
+                className="text-lg md:text-xl font-normal hover:text-primary text-black"
+              >
+                {item.item}
+              </Link>
+            </NavigationMenuItem>
           ))}
-        </ul>
-        <div className="flex items-center justify-center gap-6">
-          <button className="text-lg md:text-xl font-medium text-main-green">
-            Sign Up
-          </button>
-          <button className="cta-btn">Log In</button>
-        </div>
+        </NavigationMenuList>
+        <NavigationMenuList className="flex items-center justify-center gap-6">
+          <NavigationMenuItem>
+            <Link href={"/"}>
+              <Button variant={"link"}>Sign Up</Button>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href={"/"}>
+              <Button>Log In</Button>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
       </div>
-    </nav>
+    </NavigationMenu>
   );
 }
