259de2fb12edcc2d0982caad2c7d9dafe573c5b1
dLuxKid <adetunjimarvellous09@gmail.com>
2024-08-20 13:04:24 +0100

    refactored sign up form

--- frontend/src/components/forms/signup/signup.tsx
+++ frontend/src/components/forms/signup/signup.tsx
@@ -1,32 +1,27 @@
 import { useState } from "react";
 
 import GoBack from "@/components/back-btn";
+import { Button } from "@/components/ui/button";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Control,
+  FieldErrors,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+  UseFormRegister,
+  UseFormSetValue,
+  UseFormWatch,
+} from "react-hook-form";
+import { z } from "zod";
+
 import ContactInfo from "./steps/contact";
 import ProfessionalInfo from "./steps/professional-info";
 
-export type Doctor = {
-  contact: {
-    fullname: string;
-    DOB: Date;
-    phonenumber: string;
-    email: string;
-    state: string;
-    country: string;
-  };
-  medicalLicenseNumber: string;
-  specialization: string;
-  qualifications: {
-    degree: string;
-    school: string;
-    year: string;
-  }[];
-  experience: {
-    workplace: string;
-    positon: string;
-    from: Date;
-    to?: Date | undefined;
-  }[];
-};
+const minDate = new Date("1900-01-01");
+const maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() - 18);
 
 const initialState = {
   contact: {
@@ -39,52 +34,179 @@ const initialState = {
   },
   medicalLicenseNumber: "",
   specialization: "",
-  qualifications: [],
-  experience: [],
+  qualifications: [{ degree: "", school: "", year: "" }],
+  experience: [{ workplace: "", positon: "", from: minDate, to: minDate }],
 };
 
-type createAccountStepType = string;
+const phoneNumberPattern = /^\+?[0-9\s\-()]{7,15}$/;
+const namePattern = /[a-zA-Z\s'-]+/;
+const medicalLicensePattern = /^[A-Z]{2}\d{6}$/;
+
+const contactSchema = z.object({
+  contact: z.object({
+    fullname: z
+      .string()
+      .min(4, { message: "Your fullname is too short" })
+      .regex(namePattern, "Invalid character in your full name"),
+    DOB: z.preprocess(
+      (arg) => {
+        if (typeof arg === "string" || arg instanceof Date)
+          return new Date(arg);
+      },
+      z
+        .date()
+        .refine((date) => date >= minDate, {
+          message: `Date of birth must be after ${
+            minDate.toISOString().split("T")[0]
+          }`,
+        })
+        .refine((date) => date <= maxDate, {
+          message: "You must be at least 18 years old.",
+        })
+    ),
+    phonenumber: z.string().regex(phoneNumberPattern, "Invalid phone number"),
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email("Invalid email address"),
+    state: z.string().min(1, { message: "city is required" }),
+    country: z.string().min(1, { message: "country is required" }),
+  }),
+});
+
+const professionalDetailsSchema = z.object({
+  medicalLicenseNumber: z
+    .string()
+    .min(8, { message: "The number is too short" })
+    .max(8, { message: "The number is too long" })
+    .regex(medicalLicensePattern, "Invalid medical license number format"),
+  specialization: z.string().min(1),
+  qualifications: z.array(
+    z.object({
+      degree: z.string().min(1, { message: "Select a degree" }),
+      school: z.string().min(1, { message: "Enter your school" }),
+      year: z.string().regex(/^\d{4}$/, "Invalid year format"),
+    })
+  ),
+  experience: z.array(
+    z.object({
+      workplace: z.string().min(1, { message: "Enter your workplace" }),
+      positon: z.string().min(1, { message: "Enter your positon" }),
+      from: z.preprocess((arg) => {
+        if (typeof arg === "string" || arg instanceof Date)
+          return new Date(arg);
+      }, z.date().min(minDate, { message: "Enter your start date" })),
+      to: z.preprocess((arg) => {
+        if (typeof arg === "string" || arg instanceof Date)
+          return new Date(arg);
+      }, z.date().optional()),
+    })
+  ),
+});
+
+const stepSchemas = [contactSchema, professionalDetailsSchema];
+
+export type userSchemas = z.infer<typeof contactSchema> &
+  z.infer<typeof professionalDetailsSchema>;
+
+export type contactProps = {
+  register: UseFormRegister<userSchemas>;
+  errors: FieldErrors<userSchemas>;
+};
+
+export type ProfDetailsProps = {
+  register: UseFormRegister<userSchemas>;
+  errors: FieldErrors<userSchemas>;
+  control: Control<userSchemas, any>;
+  setValue: UseFormSetValue<userSchemas>;
+  watch: UseFormWatch<userSchemas>;
+};
 
 export default function Signup() {
-  const [userData, setUserData] = useState<Doctor>(initialState);
+  const [step, setStep] = useState<number>(0);
+  const [userData, setUserData] = useState<userSchemas>(initialState);
 
-  const [createAccountStep, setCreateAccountStep] =
-    useState<createAccountStepType>("1");
+  const methods = useForm({
+    resolver: zodResolver(stepSchemas[step]),
+    defaultValues: userData,
+  });
 
-  const nextStep = () => {
-    setCreateAccountStep((prev) => (Number(prev) + 1).toString());
-  };
-  const prevStep = () => {
-    setCreateAccountStep((prev) => (Number(prev) - 1).toString());
-  };
+  const {
+    control,
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = methods;
 
-  const createAccountStatus: {
-    [key: string]: { step: string; component: React.ReactNode };
-  } = {
-    "1": {
-      step: "Contact Details",
-      component: <ContactInfo setUserData={setUserData} next={nextStep} />,
+  const createAccountSteps = () => [
+    {
+      step: "1",
+      stepName: "Contact Details",
+      component: <ContactInfo register={register} errors={errors} />,
     },
-    "2": {
-      step: "Professional Details",
-      component: <ProfessionalInfo setUserData={setUserData} next={nextStep} />,
+    {
+      step: "2",
+      stepName: "Professional Details",
+      component: (
+        <ProfessionalInfo
+          register={register}
+          errors={errors}
+          control={control}
+          setValue={setValue}
+          watch={watch}
+        />
+      ),
     },
+  ];
+
+  const onNext: SubmitHandler<userSchemas> = (data) => {
+    console.log(data);
+    setUserData((prevData) => ({ ...prevData, ...data }));
+    if (step < stepSchemas.length - 1) {
+      setStep((prev) => prev + 1);
+      reset({ ...userData, ...data });
+    } else {
+      console.log("Form submitted:", { ...userData, ...data });
+    }
+  };
+
+  const onBack = () => {
+    if (step >= 0) {
+      setStep((prev) => prev - 1);
+      reset(userData);
+    }
   };
 
   return (
     <>
-      {Number(createAccountStep) > 1 && <GoBack onClick={prevStep} />}
+      {step > 0 && <GoBack onClick={onBack} />}
 
       <div className="flex items-center justify-center gap-1 md:gap-2 -mb-4">
         <p className="size-5 rounded-full text-xs font-medium bg-primary flex items-center justify-center text-white">
-          {createAccountStep}
+          {createAccountSteps()[step].step}
         </p>
         <span className="h-1 rounded w-12 md:w-20 bg-primary" />
         <p className="text-base md:text-lg font-medium text-black">
-          {createAccountStatus[createAccountStep].step}
+          {createAccountSteps()[step].stepName}
         </p>
       </div>
-      {createAccountStatus[createAccountStep].component}
+
+      <FormProvider {...methods}>
+        <form
+          className="flex flex-col gap-10 max-w-xl w-full"
+          onSubmit={handleSubmit(onNext)}
+        >
+          {createAccountSteps()[step].component}
+          <div className="mx-auto max-w-sm w-full">
+            <Button type="submit" className="w-full">
+              <p className="text-sm font-bold text-white">Submit</p>
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
     </>
   );
 }
--- frontend/src/components/forms/signup/steps/contact.tsx
+++ frontend/src/components/forms/signup/steps/contact.tsx
@@ -1,179 +1,103 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
-import { userDataType } from "../signup";
+import { contactProps } from "../signup";
 
-const minDate = new Date("1900-01-01");
-const maxDate = new Date();
-maxDate.setFullYear(maxDate.getFullYear() - 18);
-
-const phoneNumberPattern = /^\+?[0-9\s\-()]{7,15}$/;
-const namePattern = /[a-zA-Z\s'-]+/;
-
-type Props = {
-  setUserData: React.Dispatch<React.SetStateAction<userDataType>>;
-  next: () => void;
-};
-
-export default function ContactInfo({ setUserData, next }: Props) {
-  const contactSchema = z.object({
-    fullname: z
-      .string()
-      .min(4, { message: "Your fullname is too short" })
-      .regex(namePattern, "Invalid character in your full name"),
-    DOB: z.preprocess(
-      (arg) => {
-        if (typeof arg === "string" || arg instanceof Date)
-          return new Date(arg);
-      },
-      z
-        .date()
-        .refine((date) => date >= minDate, {
-          message: `Date of birth must be after ${
-            minDate.toISOString().split("T")[0]
-          }`,
-        })
-        .refine((date) => date <= maxDate, {
-          message: "You must be at least 18 years old.",
-        })
-    ),
-    phonenumber: z.string().regex(phoneNumberPattern, "Invalid phone number"),
-    email: z
-      .string()
-      .min(1, { message: "Email is required" })
-      .email("Invalid email address"),
-    state: z.string().min(1, { message: "city is required" }),
-    country: z.string().min(1, { message: "country is required" }),
-  });
-
-  type schema = z.infer<typeof contactSchema>;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<schema>({
-    resolver: zodResolver(contactSchema),
-  });
-
-  const onSubmit: SubmitHandler<schema> = (data) => {
-    setUserData((prev) => ({
-      ...prev,
-      contact: data,
-    }));
-    next();
-  };
-
+export default function ContactInfo({ register, errors }: contactProps) {
   return (
-    <form
-      className="flex flex-col gap-10 max-w-xl w-full"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col gap-4 items-stretch w-full">
-        <div className="flex justify-between gap-4 sm:gap-8">
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">
-              Fullname
-            </p>
-            <Input
-              type="text"
-              {...register("fullname")}
-              placeholder="John Doe"
-              className="h-[45px] bg-transparent"
-            />
-            {errors.fullname && (
-              <span className="error peer">
-                {errors.fullname.message as string}
-              </span>
-            )}
-          </label>
-        </div>
-        <div className="flex justify-between sm:flex-row flex-col gap-4 sm:gap-8">
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">Email</p>
-            <Input
-              type="email"
-              {...register("email")}
-              placeholder="user@example.com"
-              className="h-[45px] bg-transparent"
-            />
-            {errors.email && (
-              <span className="error peer">
-                {errors.email.message as string}
-              </span>
-            )}
-          </label>
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">
-              Phone number
-            </p>
-            <Input
-              type="tel"
-              {...register("phonenumber")}
-              placeholder="+234 9012345678"
-              className="h-[45px] bg-transparent"
-            />
-            {errors.phonenumber && (
-              <span className="error peer">
-                {errors.phonenumber.message as string}
-              </span>
-            )}
-          </label>
-        </div>
-        <div className="flex justify-between sm:flex-row flex-col gap-4 sm:gap-8">
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">
-              Country
-            </p>
-            <Input
-              type="text"
-              {...register("country")}
-              placeholder="Nigeria"
-              className="h-[45px] bg-transparent"
-            />
-            {errors.country && (
-              <span className="error peer">
-                {errors.country.message as string}
-              </span>
-            )}
-          </label>
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">State</p>
-            <Input
-              type="text"
-              {...register("state")}
-              placeholder="Lagos"
-              className="h-[45px] bg-transparent"
-            />
-            {errors.state && (
-              <span className="error peer">
-                {errors.state.message as string}
-              </span>
-            )}
-          </label>
-          <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
-            <p className="font-semibold text-sm text-black-secondary">
-              Date of birth
-            </p>
-            <Input
-              type="date"
-              {...register("DOB")}
-              className="h-[45px] bg-transparent"
-            />
-            {errors.DOB && (
-              <span className="error peer">{errors.DOB.message as string}</span>
-            )}
-          </label>
-        </div>
+    <div className="flex flex-col gap-4 items-stretch w-full">
+      <div className="flex justify-between gap-4 sm:gap-8">
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">Fullname</p>
+          <Input
+            type="text"
+            {...register("contact.fullname")}
+            placeholder="John Doe"
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.fullname && (
+            <span className="error peer">
+              {errors.contact.fullname.message as string}
+            </span>
+          )}
+        </label>
       </div>
-      <div className="mx-auto max-w-sm w-full">
-        <Button type="submit" className="w-full">
-          <p className="text-sm font-bold text-white">Submit</p>
-        </Button>
+      <div className="flex justify-between sm:flex-row flex-col gap-4 sm:gap-8">
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">Email</p>
+          <Input
+            type="email"
+            {...register("contact.email")}
+            placeholder="user@example.com"
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.email && (
+            <span className="error peer">
+              {errors.contact.email.message as string}
+            </span>
+          )}
+        </label>
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">
+            Phone number
+          </p>
+          <Input
+            type="tel"
+            {...register("contact.phonenumber")}
+            placeholder="+234 9012345678"
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.phonenumber && (
+            <span className="error peer">
+              {errors.contact.phonenumber.message as string}
+            </span>
+          )}
+        </label>
       </div>
-    </form>
+      <div className="flex justify-between sm:flex-row flex-col gap-4 sm:gap-8">
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">Country</p>
+          <Input
+            type="text"
+            {...register("contact.country")}
+            placeholder="Nigeria"
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.country && (
+            <span className="error peer">
+              {errors.contact.country.message as string}
+            </span>
+          )}
+        </label>
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">State</p>
+          <Input
+            type="text"
+            {...register("contact.state")}
+            placeholder="Lagos"
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.state && (
+            <span className="error peer">
+              {errors.contact.state.message as string}
+            </span>
+          )}
+        </label>
+        <label className="relative flex flex-1 flex-shrink-0 flex-col gap-2">
+          <p className="font-semibold text-sm text-black-secondary">
+            Date of birth
+          </p>
+          <Input
+            type="date"
+            {...register("contact.DOB")}
+            className="h-[45px] bg-transparent placeholder:text-gray-500"
+          />
+          {errors?.contact?.DOB && (
+            <span className="error peer">
+              {errors.contact.DOB.message as string}
+            </span>
+          )}
+        </label>
+      </div>
+    </div>
   );
 }
